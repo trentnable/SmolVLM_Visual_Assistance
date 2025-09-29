5c749373b9dd991c65616fcc0206494d34164fb3
nholland01 <noahblakeholland@gmail.com>
2025-09-29 16:13:12 -0500

    Create mainVA.py
    Creating main function for subsequent subprograms

---
+++ mainVA.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from objectify import classify_request
+from vision import fuse_yolo_midas, to_base64_img, setup_yolo, setup_midas
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+app = FastAPI()
+
+
+# Mode 1
+out1 = classify_request("Where is my water bottle?")
+yolo_model = setup_yolo("yolo11n.pt")
+midas, transform = setup_midas("MiDaS_small")
+
+@app.post("/detect")
+async def detect(request: Request):
+    # decode request
+    # run fuse_yolo_midas()
+    # return JSON

33538f6983a03509b68e5b5164637d3ed3fca427
Holland <noah.holland@siu.edu>
2025-10-08 11:41:29 -0500

    Cleared remaining files from old implementation

--- scripts/phi3_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd C:\Users\lmgre\Documents\SIU\Senior Design\landon_repo\llama.cpp\build\bin\Release
-
-call .\llama-server.exe -m "C:\Users\lmgre\Documents\SIU\Senior Design\landon_repo\models\Phi-3-mini-4k-instruct-q4.gguf" --port 8082 -ngl 99
-
-cmd

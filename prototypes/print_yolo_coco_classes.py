bba4c30c47a0c8033c0ddf49c964f1541290b648
Landon <lmgreatline5@gmail.com>
2025-10-13 15:12:55 -0500

    Removed old implementation files

--- prototypes/print_yolo_coco_classes.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-from pprint import pprint
-
-yolo_model = YOLO("yolo11n.pt")
-
-# Pretty print the class index-to-name mapping
-pprint(yolo_model.names)
--- scripts/launch_venv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM Activate the virtual environment
-call "C:\Users\lmgre\Documents\SIU\Senior Design\pranjal_repo\SmolVLM_Visual_Assistance\dependencies\Scripts\activate.bat"
-
-REM Keep the terminal open
-cmd
